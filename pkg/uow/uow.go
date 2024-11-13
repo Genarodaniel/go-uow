088c216b228b6975e217b78a8be0702c55b5248a
Daniel Genaro <danielgenaro198@gmail.com>
2024-11-13 20:10:12 -0300

    feat(transaction-study): create essential transaction handle structure

--- pkg/uow/uow.go
+++ pkg/uow/uow.go
@@ -3,6 +3,8 @@ package uow
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 )
 
 type RepositoryFactory func(tx *sql.Tx) interface{}
@@ -14,4 +16,84 @@ type UowInterface interface {
 	CommitOrRollback() error
 	Rollback() error
 	UnRegister(name string)
+}
+
+type Uow struct {
+	Db           *sql.DB
+	Tx           *sql.Tx
+	Repositories map[string]RepositoryFactory
+}
+
+func NewUow(ctx context.Context, db *sql.DB) *Uow {
+	return &Uow{
+		Db:           db,
+		Repositories: map[string]RepositoryFactory{},
+	}
+}
+
+func (u *Uow) Register(name string, fc RepositoryFactory) {
+	u.Repositories[name] = fc
+}
+
+func (u *Uow) UnRegister(name string) {
+	delete(u.Repositories, name)
+}
+
+func (u *Uow) GetRepository(ctx context.Context, name string) (interface{}, error) {
+	if u.Tx == nil {
+		tx, err := u.Db.BeginTx(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		u.Tx = tx
+	}
+
+	repo := u.Repositories[name](u.Tx)
+	return repo, nil
+}
+
+func (u *Uow) Do(ctx context.Context, fn func(Uow *Uow) error) error {
+	if u.Tx != nil {
+		return fmt.Errorf("transaction already started")
+	}
+
+	tx, err := u.Db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	u.Tx = tx
+	if err = fn(u); err != nil {
+		if errRb := u.Rollback(); errRb != nil {
+			return errors.New(fmt.Sprintf("error Rollback: %s, Error Transaction: %s", errRb.Error(), err.Error()))
+		}
+		return err
+	}
+
+	return u.CommitOrRollback()
+}
+
+func (u *Uow) Rollback() error {
+	if u.Tx == nil {
+		return errors.New("no transaction to Rollback")
+	}
+
+	if err := u.Tx.Rollback(); err != nil {
+		return err
+	}
+
+	u.Tx = nil
+	return nil
+}
+
+func (u *Uow) CommitOrRollback() error {
+	err := u.Tx.Commit()
+	if err != nil {
+		errRb := u.Rollback()
+		if errRb != nil {
+			return fmt.Errorf("error Rollback: %s, error Transaction: %s", errRb.Error(), err.Error())
+		}
+		return err
+	}
+	u.Tx = nil
+	return nil
 }
